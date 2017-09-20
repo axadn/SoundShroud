03e6aca2743ca5485e02c997467bb77420b90960
axadn <adanmarrufo@gmail.com>
2017-09-20 11:18:23 -0700

    set up rails routes for tracks

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api, defaults: {format: :json} do
-    resources :users, only: [:create]
-    resource :session, only: [:create, :destroy]
+    resources :tracks, only: [:create, :destroy, :show]
+    resources :users, only: [:create] do
+      resources :tracks, only: [:index]
+    end
+    resource :session, only: [:create, :destroy, :get]
+
   end
   get '/', to: 'root#show'
   get '/api/tracks/s3/:id', to: 'api/tracks#getS3Url'
