d46ae73a5d0e26b804065b18db32f556c55d32d9
axadn <adanmarrufo@gmail.com>
2017-09-25 13:23:38 -0700

    change REDIS_TOGO to REDIS_PROVIDER

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,3 +1,3 @@
-uri = URI.parse(ENV["REDISTOGO_URL"])
+uri = URI.parse(ENV["REDIS_PROVIDER"] || "redis://localhost:6379/")
 REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 Resque.redis = REDIS
