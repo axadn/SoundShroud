b2820b8dacd44997f5dda38cf8d5152c03c8ef3d
axadn <adanmarrufo@gmail.com>
2017-09-28 13:43:46 -0700

    styling on user show page

--- frontend/components/user/track_item.jsx
+++ frontend/components/user/track_item.jsx
@@ -3,12 +3,15 @@ import {Link} from "react-router-dom";
 import DocumentPlayButtonContainer from "../track/document_play_button_container";
 export default (props)=> (
   <div className= "track-item">
-    <img className="medium cover-art" src={props.track.img_url}></img>
+    <img className="medium cover-art" src={props.track.img_url}
+      onClick={() => location.hash =`/tracks/${props.track.id}`}></img>
     <div className="play-button-and-info">
       <DocumentPlayButtonContainer trackId={props.track.id}/>
       <div className="info">
-        <Link to={`/users/${props.track.artist_id}`}>{props.track.artist_display_name}</Link>
-        <Link to={`/tracks/${props.track.id}`}>{props.track.title}</Link>
+        <Link className="username-link"
+          to={`/users/${props.track.artist_id}`}>{props.track.artist_display_name}</Link>
+        <Link className="title-link" 
+          to={`/tracks/${props.track.id}`}>{props.track.title}</Link>
       </div>
     </div>
   </div>
