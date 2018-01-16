3608754ba0ab25b4328be92bd0839ab6202c0686
Adan Marrufo <adanmarrufo@gmail.com>
2018-01-15 21:22:33 -0800

    create search_page_container.jsx

---
+++ frontend/components/search_page/search_page_container.js
@@ -0,0 +1,41 @@
+ import {connect} from "react-redux";
+ import React from "react";
+ import {withRouter} from "react-router-dom";
+ import {SearchPage} from "search_page";
+ import {receiveMainContentLoaded,
+    receiveMainContentLoading} from "../../actions/loading_actions";
+
+ const parentMapState = state =>({
+    query: props.match.params.query
+ });
+ const parentMapDispatch = (dispatch, props) =>({
+    fetchSearch: query =>{
+        dispatch(receiveMainContentLoading());
+        dispatch(fetchSearch(
+                    query,
+                    ()=> dispatch(receiveMainContentLoaded())
+                )
+        );
+    }
+ });
+
+ const childMapState = state =>({
+    loading: state.loading.mainContent,
+    results: state.entities.searchResults
+ });
+ const childMapDispatch = dispatch({
+
+ });
+
+ class SearchPageContainer extends React.Component{
+    componentDidMount(){
+        this.props.fetchSearch(this.props.query);
+    }
+    componentWillReceiveProps(newProps){
+        this.props.fetchSearch(this.props.query);
+    }
+    render(){
+        return <ConnectedChild/>;
+    }
+ }
+ const ConnectedChild = connect(childMapState, childMapDispatch)(SearchPage);
