6b8256077a4d53f0626b788b45bbd3330b8b95a0
Adan Marrufo <adanmarrufo@gmail.com>
2019-09-27 14:35:13 +0800

    remove redux-logger from production build

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,9 +1,15 @@
 import {createStore, applyMiddleware} from "redux";
 import ReduxThunk from "redux-thunk";
 import rootReducer from "../reducers/root_reducer"
-import logger from "redux-logger";
+
 import audioPlayer from "./middleware/audio_player";
+import logger from "redux-logger";
+
+let middleware = [ReduxThunk, audioPlayer];
+if (process.env.NODE_ENV !== 'production') {
+  middleware.push(logger);
+}
 
 export default () => {
-  return createStore(rootReducer, {}, applyMiddleware(ReduxThunk, audioPlayer, logger));
+  return createStore(rootReducer, {}, applyMiddleware(...middleware));
 };
